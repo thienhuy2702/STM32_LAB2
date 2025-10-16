2567ddb273bd753bf5c44cda346455baca8f1227
thienhuy2702 <huy.tran2702@hcmut.edu.vn>
2025-10-16 12:38:22 +0700

    update test LED

--- Lab2_exercise/Core/Inc/main.h
+++ Lab2_exercise/Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_RED_Pin GPIO_PIN_3
+#define LED_RED_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
